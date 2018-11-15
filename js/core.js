305c260aafe6f59617f4059dd47cbf6cc9bc7d24
Compeador <qj6w94g0q@gmail.com>
2018-11-15 22:16:47 +0800

    update Input

--- js/core.js
+++ js/core.js
@@ -36,11 +36,12 @@ Graphics.width  = 1280;
 Graphics.height = 720;
 Graphics.padding = 32;
 Graphics.background = "assets/backgorund.png"
+Graphics.testObject = "assets/CrystalHeart.png"
 
 // Collection of all images for preloading
 Graphics.images = [
   Graphics.background,
-
+  Graphics.testObject,
 ]
 
 // Get center x-pos of screen
@@ -62,3 +63,89 @@ Graphics.appCenterWidth = function(x = 0){
 Graphics.appCenterHeight = function(y = 0){
   return (this.height - y) / 2;
 }
+//-----------------------------------------------------------------------------
+/**
+ * The static class handles the input
+ *
+ * @class Input
+ */
+function Input() {
+  throw new Error('This is a static class');
+}
+
+Input.keymap = { 
+  k0: 48, k1: 49, k2: 50, k3: 51, k4: 52, k5: 53,
+  k6: 54, k7: 55, k8: 56, k9: 57,
+  
+  kA: 65, kB: 66, kC: 67, kD: 68, kE: 69, kF: 70,
+  kG: 71, kH: 72, kI: 73, kJ: 74, kK: 75, kL: 76,
+  kM: 77, kN: 78, kO: 79, kP: 80, kQ: 81, kR: 82,
+  kS: 83, kT: 84, kU: 85, kV: 86, kW: 87, kX: 88,
+  kY: 89, kZ: 90,
+  
+  kENTER: 13,    kRETURN: 13,  kBACKSPACE: 8, kSPACE: 32,
+  kESCAPE: 27,   kESC: 27,     kSHIFT: 16,    kTAB: 9,
+  kALT: 18,      kCTRL: 17,    kDELETE: 46,   kDEL: 46,
+  kINSERT: 45,   kINS: 45,     kPAGEUP: 33,   kPUP: 33,
+  kPAGEDOWN: 34, kPDOWN: 34,   kHOME: 36,     kEND: 35,
+  kLALT: 164,    kLCTRL: 162,  kRALT: 165,    kRCTRL: 163,
+  kLSHIFT: 160,  kRSHIFT: 161,
+  
+  kLEFT: 37, kRIGHT: 39, kUP: 38, kDOWN: 40,
+  
+  kCOLON: 186,     kAPOSTROPHE: 222, kQUOTE: 222,
+  kCOMMA: 188,     kPERIOD: 190,     kSLASH: 191,
+  kBACKSLASH: 220, kLEFTBRACE: 219,  kRIGHTBRACE: 221,
+  kMINUS: 189,     kUNDERSCORE: 189, kPLUS: 187,
+  kEQUAL: 187,     kEQUALS: 187,     kTILDE: 192,
+  
+  kF1: 112,  kF2: 113,  kF3: 114, kF4: 115, kF5: 116,
+  kF6: 117,  kF7: 118,  kF8: 119, kF9: 120, kF10: 121,
+  kF11: 122, kF12: 123,
+  
+  kArrows: 224,
+}
+
+Input.key_press   = new Array(0xff);
+Input.key_trigger = new Array(0xff);
+Input.changed     = false;
+Input.reset       = false;
+
+Input.update = function(){
+  if(Input.changed){
+    Input.changed = false;
+    Input.reset   = true;
+  }
+  else if(Input.reset){
+    Input.reset = false;
+    for(i=0;i<0xff;++i){Input.key_trigger[i] = false;}
+  }
+}
+
+Input.onKeydown = event => {
+  kid = parseInt(event.which);
+  if(!Input.key_press[kid]){
+    Input.key_trigger[kid] = true;
+  }
+  Input.key_press[kid] = true;
+  Input.changed = true;
+}
+
+Input.onKeyup = event => {
+  Input.key_press[parseInt(event.which)] = false;
+  Input.changed = true;
+}
+
+Input.trigger = function(kid){
+  return Input.key_trigger[kid];
+}
+
+Input.press = function(kid){
+  return Input.key_press[kid];
+}
+
+const KeydownListener = Input.onKeydown.bind();
+const KeyupListener = Input.onKeyup.bind();
+
+window.addEventListener("keydown", Input.onKeydown);
+window.addEventListener("keyup", Input.onKeyup);
--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Start Processing
 function start(){
   draw_background();
-  draw_circle();
+  draw_object();
 }
 
 // Create pixi app
@@ -27,14 +27,32 @@ function draw_background(){
 }
 
 // test function, draw a circle
-function draw_circle(){
-  var circle = new PIXI.Graphics();
-  circle.beginFill(0x5cafe2);
-  rad = 80;
-  circle.drawCircle(0, 0, rad);
-  circle.x = Graphics.appCenterWidth(rad);
-  circle.y = Graphics.appCenterHeight(rad);
-  app.stage.addChild(circle);
+function draw_object(){
+  var sprite = new PIXI.Sprite(Cache.load_texture(Graphics.testObject));
+  sprite.x = Graphics.appCenterWidth();
+  sprite.y = Graphics.appCenterHeight();
+  sprite.anchor.set(0.5);
+  sprite.buttonMode = true;
+  sprite.interactive = true;
+  sprite.on('rightdown', function(){
+    if(sprite.scale_flag){
+      sprite.scale.x *= 0.9;
+      sprite.scale.y *= 0.9;
+      if(sprite.scale.x <= 1)sprite.scale_flag = false;
+    }
+    else{
+      sprite.scale.x *= 1.1;
+      sprite.scale.y *= 1.1;
+      if(sprite.scale.x >= 3)sprite.scale_flag = true;
+    }
+  })
+  app.stage.addChild(sprite);
+}
+
+var cnt = 0;
+
+function update_main(){
+  Input.update();
 }
 
 //-----------------------------------------------------------------------------
@@ -51,6 +69,8 @@ PIXI.utils.sayHello(type)
 
 // Create PIXI APP & Renderer
 var app = create_app();
+app.ticker.add(Input.keyListener);
+app.ticker.add(update_main);
 var renderer = PIXI.autoDetectRenderer(Graphics.width, Graphics.height);
 document.app = app;
 document.body.appendChild(app.view);
